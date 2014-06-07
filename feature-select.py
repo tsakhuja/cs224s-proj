19b49273a452c1096e1f681c38bd4421cb357e37
Tim Sakhuja <tsakhuja@stanford.edu>
2014-06-06 23:00:14 -0700

    add ref url to feature selector

--- feature-select.py
+++ feature-select.py
@@ -1,3 +1,5 @@
+# based off of
+# http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html#example-ensemble-plot-forest-importances-py
 import numpy as np
 
 from sklearn.ensemble import ExtraTreesClassifier
